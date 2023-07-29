d492514abeaab4c4d242f38240781ea58436d2e9
Seono-Na <seono5670@gmail.com>
2023-07-30 06:31:29 +0900

    router basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Hello World!</p>,
+    errorElement: <p>Not Found🫢</p>,
+  },
+  {
+    path: "/mypage",
+    element: <p>my page</p>,
+  },
+]);
 
 function App() {
-  return <p>App</p>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
